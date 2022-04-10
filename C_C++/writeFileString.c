3066404a05c7ceefc86089187ef76ff6aa87374e
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-10 17:32:49 +0000

    Update writeFileString.c

--- C_C++/writeFileString.c
+++ C_C++/writeFileString.c
@@ -5,5 +5,23 @@ int main(){
     FILE *fp;
     char fileName[20];
 
-    printf('Input name of file to write')
+    printf('Input name of file to write');
+    gets(fileName);
+
+    fp = fopen(fileName, 'w');
+    if(fp == NULL){
+
+        puts('Cannot Open or Find');
+        puts(fileName);
+        return 1;
+
+    }
+
+    while(strlen(gets(fileName))> 0 ){
+        fputs(fileName,fp);
+        fputs('\n',fp);
+
+    }
+    fclose(fp);
+    retrun 0;
 }
